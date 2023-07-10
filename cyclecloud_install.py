55465a8bc5a1e6b8e7784dd2479f043ef44d046c
Joseph Froelicher <joseph.froelicher@horizondiscovery.com>
2023-07-10 10:00:23 -0600

    update java version

--- cyclecloud_install.py
+++ cyclecloud_install.py
@@ -477,7 +477,7 @@ def install_pre_req():
 
     if "ubuntu" in str(platform.version()).lower():
         _catch_sys_error(["apt-get", "update", "-y"])
-        _catch_sys_error(["apt-get", "install", "-y", "openjdk-8-jre-headless"])
+        _catch_sys_error(["apt-get", "install", "-y", "openjdk-8-jdk"])
         _catch_sys_error(["apt-get", "install", "-y", "unzip"])
         _catch_sys_error(["apt-get", "install", "-y", "python3-venv"])
         _catch_sys_error(["apt-get", "install", "-y", "azure-cli"])
